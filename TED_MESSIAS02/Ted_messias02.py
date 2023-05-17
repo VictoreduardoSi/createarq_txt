116660dfffef5e36496dfc8e3ae02fec0ee8bd13
Victor Eduardo <127709638+VictoreduardoSi@users.noreply.github.com>
2023-05-17 00:57:09 -0300

    Add files via upload

---
+++ TED_MESSIAS02/Ted_messias02.py
@@ -0,0 +1,9 @@
+with open("'ted_messias.txt', 'w', 'encoding='UTF-8'") as fs:
+        fs.write("CRACÓVIA\n")
+        fs.write("ISTAMBUL\n")
+        fs.write("VIENA\n")
+        fs.write("ROMA\n")
+        fs.write("BERLIM\n")
+        fs.write("LISBOA\n")
+        fs.write("PRAGA\n")
+        fs.write("MADRID\n")
